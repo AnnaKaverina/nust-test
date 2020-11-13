022ef29405d86e6c6042af0633dffb7d8c6e2ba4
Anna <a.i.kaverina@mail.ru>
2020-11-13 12:48:52 +0300

    vuex correction

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,15 @@
 <template>
 	<div class="wrapper">
-		<Items 
-			v-bind:vehicles="this.$store.state.vehicles"
-		/>
-		<!-- <div>{{test()}}</div> -->
+		<Items />
 	</div>
 </template>
 
 <script>
-	import store from '@/store/index';
 	import Items from '@/components/Items';
 	export default {
-		async fetch({store}) {
-			if(store.getters['index/get'].length === 0) {
-				await store.dispatch('loadVehicles')
-			}
-		},
-		computed: {
-			vehicles: function() {
-				return this.$store.getters['loadVehicles']
-			}
-		},
-		data: () => ({
-		}),
-		methods: {
-			test: function() {
-				this.$store.dispatch('loadVehicles')
+		mounted() {
+			if(this.$store.getters.getVehiclesFromState.length === 0) {
+				this.$store.dispatch('loadVehicles');
 			}
 		}
 	}
--- store/index.js
+++ store/index.js
@@ -1,65 +1,34 @@
-import Vue from 'vue';
 import Vuex from 'vuex';
-
-Vue.use(Vuex);
-
 import {getVehicles} from './request';
 
 const store = () => new Vuex.Store({
-    
     state: {
         vehicles: [],
         select: ['whatever', 'airship', 'rocket', 'helicopter']
     },
+    
     getters: {
-        getVehicles(state) {
+        getVehiclesFromState(state) {
             return state.vehicles;
+        },
+        getSelect(state) {
+            return state.select;
         }
     },
+    
     mutations: {
         setVehicles(state, value) {
             state.vehicles = value;
         }
     },
+    
     actions: {
-        loadVehicles({commit}) {
-            getVehicles()
-            .then(json => commit('setVehicles', json))
-            .catch(() => {
-                $nuxt.error({ message: 'Server error', statusCode: 404 });
-            });
+        async loadVehicles(context) {
+            const res = await getVehicles();
+            context.commit('setVehicles', res);
         }
     }
 });
 
 export default store;
 
-/* export const state = () => ({
-    vehicles: [],
-    select: ['whatever', 'airship', 'rocket', 'helicopter']
-});
-
-export const mutations = {
-    setVehicles(state, vehicles) {
-        state.vehicles = vehicles;
-    }
-};
-
-import getVehicles from './request';
-
-export const actions = {
-    async loadVehicles({commit}) {
-        getVehicles()
-        .then(vehicles => commit('setVehicles', vehicles))
-        .catch((error) => {
-            $nuxt.error({ message: 'Server error', statusCode: 404 });
-            throw error;
-        });
-    }
-};
-
-export const getters = {
-    get: state => {
-        return state.vehicles;
-    }
-}; */
